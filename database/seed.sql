f4a6843c6d0ff93d6d143b8cc47f096396e9a9dc
Daniel Pludowski <daniel.pludowski@nimbleapproach.com>
2025-02-07 09:51:45 +0000

    Corrections: dup yaml and reverting seed test user

--- database/seed.sql
+++ database/seed.sql
@@ -9,8 +9,7 @@ INSERT INTO users (username, email, password) VALUES
 ('user7', 'user7@example.com', 'scrypt:32768:8:1$v6ML5QfsEKXLRIGj$5b5a813e8117cbc5f9c798ece1c22f40edc716b7aa429c82a5057dd1ac9ccd5de70600166cdbc5fa7954931eed6b8f090ea4a6efd2a1ef4ef98e38f3820e32fb'),
 ('user8', 'user8@example.com', 'scrypt:32768:8:1$HbbweP13wMLEorQP$f018448f48ed4286b5528eb782b444637af03bc7c7839ca249f6b63143a91d5cf14e58846bd77a4c704348dabf45be13aa9d68c77666d6f3ebb90828f39f289c'),
 ('user9', 'user9@example.com', 'scrypt:32768:8:1$Q1YBWVoQYKrS2dtj$2d059dd481df84b1e69db1db11634e4a8a8835110da5d6ba7aac7b8449b7f71f22c02d8da505d1b5f771930502ae21b52a4ae684197855f68fe88f1c909d2ed0'),
-('user10', 'user10@example.com', 'scrypt:32768:8:1$bhUlzECJwTv502gA$8a8b24fbfd4181c5930762f459c811b446b403ecb47e1efc4efa56d987210c1bae5e1ffd64f68176b2aa0684542fe9f599cdfc6e9ef7ff1778a8ecaf2eba4b59'),
-('testuser', 'testuser@example.com', 'scrypt:32768:8:1$9sxOlox0BtoiKQeV$d9271c470dbcee2d30e08a2fc5e568576d3a6b9c9fa595a20d249af7e13147af3f7a17457d8c6f469fb1027af971f8bf2d64de784c7187b2fe0cc4487d160901');
+('user10', 'user10@example.com', 'scrypt:32768:8:1$bhUlzECJwTv502gA$8a8b24fbfd4181c5930762f459c811b446b403ecb47e1efc4efa56d987210c1bae5e1ffd64f68176b2aa0684542fe9f599cdfc6e9ef7ff1778a8ecaf2eba4b59');
 
 -- Insert 20 transactions for each user
 DO $$
